f6c9091600cb9ef7af8aa84f7d8438c46553fc22
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2025-11-19 22:48:20 +0900

    ocr 추가
    Signed-off-by: Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 from enum import Enum, auto
 
+import easyocr
+
 from kakao import get_kakao_plus_friend_profiles, get_kakao_plus_friend_posts
 
 
@@ -8,11 +10,18 @@ class MenuSource(Enum):
     KAKAO_PLUS_FRIEND_POST = auto()
 
 
+def ocr(image_url: str):
+    reader = easyocr.Reader(['ko'])
+    text = reader.readtext(image_url)
+    return ' '.join(map(lambda x: x[1], text))
+
+
 def get_menu_from_kakao_profile(pf_id: str):
     profile = get_kakao_plus_friend_profiles(pf_id)
     profile_card = filter(lambda x: x.type == 'profile', profile.cards).__next__()
-    menu = profile_card.profile.profile_image.url
-    return menu
+    menu_image_url = profile_card.profile.profile_image.url
+    menu_text = ocr(menu_image_url)
+    return menu_text + ' ' + menu_image_url
 
 
 def get_menu_from_kakao_post(pf_id: str) -> str:
