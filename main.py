a10ff9416c04600c695a4010297d5b0842315e53
Seokjin Yoon <13289043+sjy5386@users.noreply.github.com>
2026-01-28 20:47:17 +0900

    feat: 특정 식당 수동 실행을 위한 워크플로우 추가 및 관련 로직 수정

--- main.py
+++ main.py
@@ -7,8 +7,20 @@ from slack import send_slack_webhook, SlackWebhookPayload
 
 if __name__ == '__main__':
     print(datetime.datetime.now())
-    menu_frequency = MenuFrequency[os.environ.get('MENU_FREQUENCY', MenuFrequency.DAILY.name)]
-    for restaurant in filter(lambda x: x.menu_frequency == menu_frequency, Restaurant):
+    menu_frequency_env = os.environ.get('MENU_FREQUENCY')
+    restaurant_env = os.environ.get('RESTAURANT')
+
+    if restaurant_env:
+        restaurants = [Restaurant[restaurant_env]]
+    elif menu_frequency_env:
+        menu_frequency = MenuFrequency[menu_frequency_env]
+        restaurants = list(filter(lambda x: x.menu_frequency == menu_frequency, Restaurant))
+    else:
+        # Default behavior if nothing is specified
+        menu_frequency = MenuFrequency.DAILY
+        restaurants = list(filter(lambda x: x.menu_frequency == menu_frequency, Restaurant))
+
+    for restaurant in restaurants:
         menu = restaurant.get_menu()
         print(restaurant.name, menu)
         slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL')
